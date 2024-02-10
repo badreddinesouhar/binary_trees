3925126eace79142c12f1b654709be829a31ade1
badreddinesouhar <badrsouhar@gmail.com>
2024-02-10 21:06:29 +0100

    insert in the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
+    if (parent == NULL) {
+        return NULL;
+    }
+
+    binary_tree_t *left = binary_tree_node(parent, value);
+    if (left == NULL) {
+        return NULL;
+    }
+
+    if (parent->left != NULL) {
+        parent->left->parent = left;
+        left->left = parent->left;
+    }
+
+    parent->left = left;
+
+    return left;
+}
+
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 12);
+    root->right = binary_tree_node(root, 402);
+    binary_tree_print(root);
+    printf("\n");
+    binary_tree_insert_left(root->right, 128);
+    binary_tree_insert_left(root, 54);
+    binary_tree_print(root);
+    return (0);
+}
